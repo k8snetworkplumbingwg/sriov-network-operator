4c26054395892f6ce5c7db4531a15fd3f90c292b
Zenghui Shi <zshi@redhat.com>
2021-03-16 16:10:19 +0800

    Remove mco plugin
    Signed-off-by: Zenghui Shi <zshi@redhat.com>

--- pkg/daemon/daemon.go
+++ pkg/daemon/daemon.go
@@ -489,7 +489,6 @@ func (dn *Daemon) nodeStateSyncHandler(generation int64) error {
 
 	reqReboot := false
 	reqDrain := false
-	mcoReboot := false
 	for k, p := range dn.LoadedPlugins {
 		d, r := false, false
 		if dn.nodeState.GetName() == "" {
@@ -501,11 +500,6 @@ func (dn *Daemon) nodeStateSyncHandler(generation int64) error {
 			glog.Errorf("nodeStateSyncHandler(): plugin %s error: %v", k, err)
 			return err
 		}
-
-		if p.Name() == "mco_plugin" && d && !r {
-			mcoReboot = true
-		}
-
 		glog.V(0).Infof("nodeStateSyncHandler(): plugin %s: reqDrain %v, reqReboot %v", k, d, r)
 		reqDrain = reqDrain || d
 		reqReboot = reqReboot || r
@@ -513,7 +507,7 @@ func (dn *Daemon) nodeStateSyncHandler(generation int64) error {
 	glog.V(0).Infof("nodeStateSyncHandler(): reqDrain %v, reqReboot %v disableDrain %v", reqDrain, reqReboot, dn.disableDrain)
 
 	for k, p := range dn.LoadedPlugins {
-		if k != GenericPlugin && k != McoPlugin {
+		if k != GenericPlugin {
 			err := p.Apply()
 			if err != nil {
 				glog.Errorf("nodeStateSyncHandler(): plugin %s fail to apply: %v", k, err)
@@ -521,15 +515,7 @@ func (dn *Daemon) nodeStateSyncHandler(generation int64) error {
 			}
 		}
 	}
-	if mcoReboot {
-		glog.V(0).Infof("nodeStateSyncHandler(): MCO will trigger reboot")
-		err = dn.LoadedPlugins[McoPlugin].Apply()
-		if err != nil {
-			glog.Errorf("nodeStateSyncHandler(): mco_plugin fail to apply: %v", err)
-			return err
-		}
-		return nil
-	}
+
 	if utils.ClusterType == utils.ClusterTypeOpenshift {
 		if err = dn.getNodeMachinePool(); err != nil {
 			return err
@@ -674,9 +660,7 @@ func (dn *Daemon) loadVendorPlugins(ns *sriovnetworkv1.SriovNetworkNodeState) er
 		pl = append(pl, VirtualPlugin)
 	} else {
 		pl = registerPlugins(ns)
-		if utils.ClusterType == utils.ClusterTypeOpenshift {
-			pl = append(pl, McoPlugin)
-		} else {
+		if utils.ClusterType != utils.ClusterTypeOpenshift {
 			pl = append(pl, K8sPlugin)
 		}
 		pl = append(pl, GenericPlugin)
