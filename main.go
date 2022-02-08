bcb8935f45eebb49ad45cc5e9d1507b07d17b59f
Peng Liu <pliu@redhat.com>
2022-02-08 03:01:42 -0500

    Add openshift/api/config CRD to scheme
    As we use configv1.Infrastructure in function configv1.Infrastructure.

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ import (
 	"os"
 
 	netattdefv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
+	configv1 "github.com/openshift/api/config/v1"
 	mcfgv1 "github.com/openshift/machine-config-operator/pkg/apis/machineconfiguration.openshift.io/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 
@@ -59,6 +60,7 @@ func init() {
 	utilruntime.Must(sriovnetworkv1.AddToScheme(scheme))
 	utilruntime.Must(netattdefv1.AddToScheme(scheme))
 	utilruntime.Must(mcfgv1.AddToScheme(scheme))
+	utilruntime.Must(configv1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -238,7 +240,7 @@ func createDefaultOperatorConfig(c client.Client) error {
 	logger := setupLog.WithName("createDefaultOperatorConfig")
 	singleNode, err := utils.IsSingleNodeCluster(c)
 	if err != nil {
-		return fmt.Errorf("Couldn't check the amount of nodes in the cluster")
+		return fmt.Errorf("couldn't check the amount of nodes in the cluster: %v", err)
 	}
 
 	enableAdmissionController := os.Getenv("ENABLE_ADMISSION_CONTROLLER") == "true"
