fb6bd635e37afba35ef36085fe771b58fe5c030b
Yury Kulazhenkov <ykulazhenkov@nvidia.com>
2024-03-04 16:24:04 +0200

    Add implementation for OVSNetwork controller
    Signed-off-by: Yury Kulazhenkov <ykulazhenkov@nvidia.com>

--- main.go
+++ main.go
@@ -141,6 +141,15 @@ func main() {
 		os.Exit(1)
 	}
 
+	err = mgrGlobal.GetCache().IndexField(context.Background(), &sriovnetworkv1.OVSNetwork{}, "spec.networkNamespace", func(o client.Object) []string {
+		return []string{o.(*sriovnetworkv1.OVSNetwork).Spec.NetworkNamespace}
+	})
+
+	if err != nil {
+		setupLog.Error(err, "unable to create index field for cache")
+		os.Exit(1)
+	}
+
 	if err := initNicIDMap(); err != nil {
 		setupLog.Error(err, "unable to init NicIdMap")
 		os.Exit(1)
@@ -170,6 +179,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "SriovIBNetwork")
 		os.Exit(1)
 	}
+	if err = (&controllers.OVSNetworkReconciler{
+		Client: mgrGlobal.GetClient(),
+		Scheme: mgrGlobal.GetScheme(),
+	}).SetupWithManager(mgrGlobal); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "OVSNetwork")
+		os.Exit(1)
+	}
 	if err = (&controllers.SriovNetworkNodePolicyReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
@@ -191,13 +207,6 @@ func main() {
 		PlatformHelper: platformsHelper,
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "SriovNetworkPoolConfig")
-		os.Exit(1)
-	}
-	if err = (&controllers.OVSNetworkReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "OVSNetwork")
 		os.Exit(1)
 	}
 
