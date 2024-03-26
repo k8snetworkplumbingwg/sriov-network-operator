aa49551c65f4b0f0ab2925bcdcbcadca08f09813
amaslennikov <amaslennikov@nvidia.com>
2024-03-26 10:43:37 +0300

    Add GUID type and methods to utils
    GUID type allows to switch seamlessly between GUID representations
    in string and net.HardwareAddr forms and apply arithmetic operations
    
    Signed-off-by: amaslennikov <amaslennikov@nvidia.com>

---
+++ pkg/utils/guid.go
@@ -0,0 +1,48 @@
+package utils
+
+import (
+	"fmt"
+	"net"
+)
+
+// GUID address is an uint64 encapsulation for network hardware address
+type GUID uint64
+
+const (
+	guidLength = 8
+	byteBitLen = 8
+	byteMask   = 0xff
+)
+
+// ParseGUID parses string only as GUID 64 bit
+func ParseGUID(s string) (GUID, error) {
+	ha, err := net.ParseMAC(s)
+	if err != nil {
+		return 0, err
+	}
+	if len(ha) != guidLength {
+		return 0, fmt.Errorf("invalid GUID address %s", s)
+	}
+	var guid uint64
+	for idx, octet := range ha {
+		guid |= uint64(octet) << uint(byteBitLen*(guidLength-1-idx))
+	}
+	return GUID(guid), nil
+}
+
+// String returns the string representation of GUID
+func (g GUID) String() string {
+	return g.HardwareAddr().String()
+}
+
+// HardwareAddr returns GUID representation as net.HardwareAddr
+func (g GUID) HardwareAddr() net.HardwareAddr {
+	value := uint64(g)
+	ha := make(net.HardwareAddr, guidLength)
+	for idx := guidLength - 1; idx >= 0; idx-- {
+		ha[idx] = byte(value & byteMask)
+		value >>= byteBitLen
+	}
+
+	return ha
+}
