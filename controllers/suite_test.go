686a48c866cdda9ae04ec8703e80690d2071a3d4
Andrea Panattoni <apanatto@redhat.com>
2024-05-28 11:26:07 +0200

    metrics-exporter: configure Prometheus operator
    Deploy the needed configuration to make the prometheus
    operator to find and scrape the sriov-network-metrics-exporter
    endpoints, including the ServiceMonitor, Role and RoleBinding.
    
    Resources are installed only if the Prometheus operator is installed.
    
    Signed-off-by: Andrea Panattoni <apanatto@redhat.com>

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -41,6 +41,7 @@ import (
 	netattdefv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
 	openshiftconfigv1 "github.com/openshift/api/config/v1"
 	mcfgv1 "github.com/openshift/machine-config-operator/pkg/apis/machineconfiguration.openshift.io/v1"
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 
 	//+kubebuilder:scaffold:imports
 	sriovnetworkv1 "github.com/k8snetworkplumbingwg/sriov-network-operator/api/v1"
@@ -156,6 +157,8 @@ var _ = BeforeSuite(func() {
 	err = mcfgv1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 	err = openshiftconfigv1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+	err = monitoringv1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
 	vars.Config = cfg
