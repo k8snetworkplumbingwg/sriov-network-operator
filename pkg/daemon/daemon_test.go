2c3607c47c4cc6bcd17410f857f1112034b95c3b
Ivan Kolodiazhnyi <ikolodiazhny@nvidia.com>
2025-04-28 12:57:52 +0300

    Make kargs.sh work with systemd service and for Ubuntu
    Signed-off-by: Ivan Kolodiazhnyi <ikolodiazhny@nvidia.com>

--- pkg/daemon/daemon_test.go
+++ pkg/daemon/daemon_test.go
@@ -52,7 +52,7 @@ var (
 )
 
 const (
-	waitTime  = 30 * time.Minute
+	waitTime  = 2 * time.Minute
 	retryTime = 5 * time.Second
 )
 
@@ -120,6 +120,7 @@ var _ = Describe("Daemon Controller", Ordered, func() {
 		// general
 		hostHelper.EXPECT().Chroot(gomock.Any()).Return(func() error { return nil }, nil).AnyTimes()
 		hostHelper.EXPECT().RunCommand("/bin/sh", gomock.Any(), gomock.Any(), gomock.Any()).Return("", "", nil).AnyTimes()
+		hostHelper.EXPECT().RunCommand("/bin/bash", gomock.Any(), gomock.Any(), gomock.Any()).Return("", "", nil).AnyTimes()
 
 		discoverSriovReturn.Store(&[]sriovnetworkv1.InterfaceExt{})
 
