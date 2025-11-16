211ab3f1cb5a79ca2f4b212be77319a98469fa6e
Sebastian Sch <sebassch@gmail.com>
2025-11-16 11:33:56 +0200

    Add helper function for virtual environments to find vfs
    Signed-off-by: Sebastian Sch <sebassch@gmail.com>

--- pkg/helper/mock/mock_helper.go
+++ pkg/helper/mock/mock_helper.go
@@ -344,6 +344,21 @@ func (mr *MockHostHelpersInterfaceMockRecorder) DiscoverSriovDevices(storeManage
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "DiscoverSriovDevices", reflect.TypeOf((*MockHostHelpersInterface)(nil).DiscoverSriovDevices), storeManager)
 }
 
+// DiscoverSriovVirtualDevices mocks base method.
+func (m *MockHostHelpersInterface) DiscoverSriovVirtualDevices() ([]v1.InterfaceExt, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "DiscoverSriovVirtualDevices")
+	ret0, _ := ret[0].([]v1.InterfaceExt)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// DiscoverSriovVirtualDevices indicates an expected call of DiscoverSriovVirtualDevices.
+func (mr *MockHostHelpersInterfaceMockRecorder) DiscoverSriovVirtualDevices() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "DiscoverSriovVirtualDevices", reflect.TypeOf((*MockHostHelpersInterface)(nil).DiscoverSriovVirtualDevices))
+}
+
 // DiscoverVDPAType mocks base method.
 func (m *MockHostHelpersInterface) DiscoverVDPAType(pciAddr string) string {
 	m.ctrl.T.Helper()
